464acbf59cf621b009525696b2ef4648bd5d79d1
Zuo ZongMing <zuozongming@cloudin.ren>
2016-11-02 16:01:01 +0800

    demo

---
+++ net_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+###########################
+#version 0.1:
+# test of The network is available
+# zuozongming by 2016/10/11
+##########################
+
+# Var
+
+
+# Functions
+
+# Begin script
+
+
+# End script
+
